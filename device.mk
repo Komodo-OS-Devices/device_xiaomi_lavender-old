afd38687e66038aaec4da39183d947a97545e432
rena2806 <renascape@gmail.com>
2020-04-18 10:56:22 +0200

    lavender: Build Google Camera GO

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    Snap
+    CameraGo
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
