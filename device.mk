675a41fb0fb555ed8fd640890b4286b72c425ea1
alanndz <alanmahmud0@gmail.com>
2020-08-07 02:16:55 +0000

    lavender: device: add curl and nano for build
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- device.mk
+++ device.mk
@@ -275,6 +275,11 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+# Other
+PRODUCT_PACKAGES += \
+    curl \
+    nano
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
